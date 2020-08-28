1807fa10db3699c94baa96be1fcd70e9d706cc8f
yinxingpan <yinxingpan@aegis-data.cn>
2020-08-28 14:56:57 +0800

    add file

--- xiaofa/actions.py
+++ xiaofa/actions.py
@@ -25,3 +25,140 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+# -*- coding: utf-8 -*-
+import logging
+import json
+import requests
+from datetime import datetime
+from typing import Any, Dict, List, Text, Union, Optional
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+from rasa_sdk.events import (
+    SlotSet,
+    UserUtteranceReverted,
+    ConversationPaused,
+    EventType,
+    FollowupAction,
+)
+
+from actions.api.gdrive_service import GDriveService
+
+
+logger = logging.getLogger(__name__)
+
+
+class SalesForm(FormAction):
+    """Collects sales information and adds it to the spreadsheet"""
+
+    def name(self) -> Text:
+        return "sales_form"
+
+    @staticmethod
+    def required_slots(tracker) -> List[Text]:
+        return [
+            "job_function",
+            "use_case",
+            "budget",
+            "person_name",
+            "company",
+            "business_email",
+        ]
+
+    def submit(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        dispatcher.utter_message("Thanks for getting in touch, we’ll contact you soon")
+        return []
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict[Text, Any]]]]:
+        """A dictionary to map required slots to
+        - an extracted entity
+        - intent: value pairs
+        - a whole message
+        or a list of them, where a first match will be picked"""
+        return {"use_case": self.from_text(intent="inform")}
+
+    # def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+    #     """A dictionary to map required slots to
+    #         - an extracted entity
+    #         - intent: value pairs
+    #         - a whole message
+    #         or a list of them, where a first match will be picked"""
+    #
+    #     return {
+    #         "job_function": [
+    #             self.from_entity(entity="job_function"),
+    #             self.from_text(intent="enter_data"),
+    #         ],
+    #         "use_case": [self.from_text(intent="enter_data")],
+    #         "budget": [
+    #             self.from_entity(entity="amount-of-money"),
+    #             self.from_entity(entity="number"),
+    #             self.from_text(intent="enter_data"),
+    #         ],
+    #         "person_name": [
+    #             self.from_entity(entity="name"),
+    #             self.from_text(intent="enter_data"),
+    #         ],
+    #         "company": [
+    #             self.from_entity(entity="company"),
+    #             self.from_text(intent="enter_data"),
+    #         ],
+    #         "business_email": [
+    #             self.from_entity(entity="email"),
+    #             self.from_text(intent="enter_data"),
+    #         ],
+    #     }
+    #
+    # def validate_business_email(
+    #     self, value, dispatcher, tracker, domain
+    # ) -> Dict[Text, Any]:
+    #     """Check to see if an email entity was actually picked up by duckling."""
+    #
+    #     if any(tracker.get_latest_entity_values("email")):
+    #         # entity was picked up, validate slot
+    #         return {"business_email": value}
+    #     else:
+    #         # no entity was picked up, we want to ask again
+    #         dispatcher.utter_message(template="utter_no_email")
+    #         return {"business_email": None}
+    #
+    # def submit(
+    #     self,
+    #     dispatcher: CollectingDispatcher,
+    #     tracker: Tracker,
+    #     domain: Dict[Text, Any],
+    # ) -> List[EventType]:
+    #     """Once we have all the information, attempt to add it to the
+    #     Google Drive database"""
+    #
+    #     import datetime
+    #
+    #     budget = tracker.get_slot("budget")
+    #     company = tracker.get_slot("company")
+    #     email = tracker.get_slot("business_email")
+    #     job_function = tracker.get_slot("job_function")
+    #     person_name = tracker.get_slot("person_name")
+    #     use_case = tracker.get_slot("use_case")
+    #     date = datetime.datetime.now().strftime("%d/%m/%Y")
+    #
+    #     sales_info = [company, use_case, budget, date, person_name, job_function, email]
+    #
+    #     try:
+    #         gdrive = GDriveService()
+    #         gdrive.store_data(sales_info)
+    #         dispatcher.utter_message(template="utter_confirm_salesrequest")
+    #         return []
+    #     except Exception as e:
+    #         logger.error(
+    #             "Failed to write data to gdocs. Error: {}" "".format(e.message),
+    #             exc_info=True,
+    #         )
+    #         dispatcher.utter_message(template="utter_salesrequest_failed")
+    #         return []
